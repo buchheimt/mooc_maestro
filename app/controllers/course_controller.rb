8b2aa93650c1309b80baa92cfad3cd1cc18e59c8
Tyler <tbuchhei@alumni.nd.edu>
2017-09-08 16:38:47 -0500

    add functionality for course signup

--- app/controllers/course_controller.rb
+++ app/controllers/course_controller.rb
@@ -1,8 +1,31 @@
 class CourseController < ApplicationController
 
   get '/courses' do
-    @courses = Course.all.sort {|a,b| a.name <=> b.name}
-    erb :'courses/index'
+    if logged_in?
+      @courses = Course.all.sort {|a,b| a.name <=> b.name}
+      erb :'courses/index'
+    else
+      redirect '/users/login'
+    end
+  end
+
+  get '/courses/new' do
+    if logged_in?
+      erb :'courses/new'
+    else
+      redirect '/users/login'
+    end
+  end
+
+  post '/courses' do
+    if !logged_in? || params[:course][:name].empty? || User.find_by(username: params[:course][:name])
+      redirect '/courses/new'
+    else
+      @user = current_user
+      @course = @user.courses.build(params[:course])
+      @user.save
+      redirect '/users/homepage'
+    end
   end
 
 end
