27dedf4a049b5995d57e61eded6eaaae09403c3b
Tyler <tbuchhei@alumni.nd.edu>
2017-09-09 17:26:25 -0500

    add working subject show and index, add slug methods

--- app/controllers/subject_controller.rb
+++ app/controllers/subject_controller.rb
@@ -9,4 +9,17 @@ class SubjectController < ApplicationController
     end
   end
 
+  get '/subjects/new' do
+
+  end
+
+  get '/subjects/:slug' do
+    @subject = Subject.find_by_slug(params[:slug])
+    if @subject && logged_in?
+      erb :'subjects/show'
+    else
+      redirect '/users/login'
+    end
+  end
+
 end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -6,4 +6,12 @@ class Subject < ActiveRecord::Base
   has_many :platforms, through: :programs
   has_many :users, through: :courses
 
+  def slug
+    return self.name.downcase.split.join("-")
+  end
+
+  def self.find_by_slug(slug)
+    self.all.detect {|program| program.slug == slug}
+  end
+
 end
