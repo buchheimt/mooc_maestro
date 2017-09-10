f651feb9a2e6b861a74ca468d5cc0a35ee0ddf7a
Tyler <tbuchhei@alumni.nd.edu>
2017-09-10 09:39:25 -0500

    platform editing functional

--- app/controllers/platform_controller.rb
+++ app/controllers/platform_controller.rb
@@ -43,10 +43,28 @@ class PlatformController < ApplicationController
     @platform = Platform.find_by_slug(params[:slug])
     if @platform && user_created?(@platform)
       @programs = Program.all.select {|pr| pr.name == "Unassigned" || pr.platform == @platform}
-      binding.pry
       erb :'platforms/edit'
     else
       redirect '/platforms'
+    end
+  end
+
+  patch '/platforms/:slug' do
+    @platform = Platform.find_by_slug(params[:slug])
+    @new_name = params[:platform][:name]
+    if @platform && user_created?(@platform) && ! @new_name.empty?
+      if @new_name != @platform.name && Platform.find_by(name: @new_name)
+        redirect '/platforms/<%= @platform.slug %>/edit'
+      else
+        @platform.name = @new_name
+        @platform.description = params[:platform][:description] unless params[:platform][:description].empty?
+        @platform.programs.clear
+        params[:platform][:program_ids].each {|pr_id| @platform.programs << Program.find(pr_id)}
+        @platform.save
+        redirect "/platforms/#{@platform.slug}"
+      end
+    else
+      redirect '/platforms/<%= @platform.slug %>/edit'
     end
   end
 
