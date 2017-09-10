4bac93ab9616a328691f1f6498cf5102e7c175d4
Tyler <tbuchhei@alumni.nd.edu>
2017-09-10 12:33:16 -0500

    add program edit functionality and fix platform edit

--- app/controllers/platform_controller.rb
+++ app/controllers/platform_controller.rb
@@ -42,7 +42,7 @@ class PlatformController < ApplicationController
   get '/platforms/:slug/edit' do
     @platform = Platform.find_by_slug(params[:slug])
     if @platform && user_created?(@platform)
-      @programs = Program.all.select {|pr| pr.name == "Unassigned" || pr.platform == @platform}
+      @programs = Program.all.select {|pr| pr.platform.name == "Unassigned" || pr.platform == @platform}.reject {|pr| pr.name == "Individual Courses"}
       erb :'platforms/edit'
     else
       redirect '/platforms'
@@ -54,17 +54,21 @@ class PlatformController < ApplicationController
     @new_name = params[:platform][:name]
     if @platform && user_created?(@platform) && ! @new_name.empty?
       if @new_name != @platform.name && Platform.find_by(name: @new_name)
-        redirect '/platforms/<%= @platform.slug %>/edit'
+        redirect "/platforms/#{@platform.slug}/edit"
       else
         @platform.name = @new_name
         @platform.description = params[:platform][:description] unless params[:platform][:description].empty?
         @platform.programs.clear
         params[:platform][:program_ids].each {|pr_id| @platform.programs << Program.find(pr_id)}
+        Program.all.select {|pr| pr.platform_id == nil}.each do |pr|
+          pr.platform = Platform.find_by(name: "Unassigned")
+          pr.save
+        end
         @platform.save
         redirect "/platforms/#{@platform.slug}"
       end
     else
-      redirect '/platforms/<%= @platform.slug %>/edit'
+      redirect "/platforms/#{@platform.slug}/edit"
     end
   end
 
--- app/controllers/program_controller.rb
+++ app/controllers/program_controller.rb
@@ -47,6 +47,40 @@ class ProgramController < ApplicationController
     end
   end
 
+  get '/programs/:slug/edit' do
+    @program = Program.find_by_slug(params[:slug])
+    if @program && user_created?(@program)
+      @platforms = Platform.all.reject {|pl| pl.name == "Unassigned"}
+      @courses = Course.all.select {|c| c.program.name == "Individual Courses" || c.program == @program}
+      erb :'programs/edit'
+    else
+      redirect '/programs'
+    end
+  end
+
+  patch '/programs/:slug' do
+    @program = Program.find_by_slug(params[:slug])
+    @new_name = params[:program][:name]
+    if @program && user_created?(@program) && ! @new_name.empty?
+      if @new_name != @program.name && Program.find_by(name: @new_name)
+        redirect "/programs/#{@program.slug}/edit"
+      else
+        @program.name = @new_name
+        @program.description = params[:program][:description] unless params[:program][:description].empty?
+        @program.courses.clear
+        params[:program][:course_ids].each {|c_id| @program.courses << Course.find(c_id)}
+        Course.all.select {|c| c.program_id == nil}.each do |c|
+          c.program = Program.find_by(name: "Individual Courses")
+          c.save
+        end
+        @program.save
+        redirect "/programs/#{@program.slug}"
+      end
+    else
+      redirect "/programs/#{@program.slug}/edit"
+    end
+  end
+
   get '/programs/:slug' do
     @program = Program.find_by_slug(params[:slug])
     if @program && logged_in?
